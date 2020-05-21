1a088da827462aaca96593373ce1be5bc8827527
zjr1234 <64582965+zjr1234@users.noreply.github.com>
2020-05-21 16:14:42 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import time
 
-print "Start : %s" % time.ctime()
+print ("Start : %s" % time.ctime())
 time.sleep( 60 )
-print "End : %s" % time.ctime()
+print ("End : %s" % time.ctime())
